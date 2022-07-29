f8ea9f3f8e0dad69a65b3769ff00509cfe88c83e
&! Tekky#1337 <98614666+xtekky@users.noreply.github.com>
2022-07-29 17:46:54 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,6 @@
 // Xlog v01
+// @price: 100€
+// @contact: t.me/xtekky
 
 package main
 
@@ -14,10 +16,7 @@ var sbox = []int{
 }
 
 var blowfishSub = []int{
-	0x9C30D539,
-	0x2AF26013,
-	0xC5D1B023,
-	0x286085F0,
+	// removed from preview
 }
 
 var blowfishP = []int{
@@ -25,17 +24,7 @@ var blowfishP = []int{
 }
 
 func subDword(x int) int {
-	a := (x >> 24) & 0xFF
-	b := (x >> 16) & 0xFF
-	c := (x >> 8) & 0xFF
-	d := x & 0xFF
-	y := sbox[d]
-	y |= sbox[c] << 8
-	y |= sbox[b] << 16
-	y |= sbox[a] << 24
-	u := ((y << 13) | (y >> 19)) & 0xFFFFFFFF
-	v := ((y << 23) | (y >> 9)) & 0xFFFFFFFF
-	return y ^ u ^ v
+	// removed from preview
 }
 
 func keySchedule(initialKey []uint32) []uint32 {
@@ -47,18 +36,7 @@ func subDwordRot(x uint32) int {
 }
 
 func obfuscDword(inval uint32) uint32 {
-	bss := new(bytes.Buffer)
-	_ = binary.Write(bss, binary.LittleEndian, inval)
-
-	bs := bss.Bytes()
-
-	magic := 0x4E67C6A7
-	out := weh(uint32(magic), uint32(bs[0])) ^ uint32(magic)
-	out = weh(out, uint32(bs[1])) ^ uint32(out)
-	out = weh(out, uint32(bs[2])) ^ uint32(out)
-	out = weh(out, uint32(bs[3])) ^ uint32(out)
-
-	return out
+	// removed from preview
 }
 
 func mixRowApplyKey(bs []byte, rks []uint32) []uint32 {
@@ -66,52 +44,24 @@ func mixRowApplyKey(bs []byte, rks []uint32) []uint32 {
 }
 
 func reverseByteBits(x uint32) uint32 {
-	a := ((x >> 1) & 0x55555555) | ((x << 1) & 0xAAAAAAAA)
-	b := ((a >> 2) & 0x33333333) | ((a << 2) & 0xCCCCCCCC)
-	res := uint32((b>>4)&0xF0F0F0F) | uint32((b<<4)&0xF0F0F0F0)
-	return res
+	// removed from preview
 }
 
 func mixNibbles(x uint32) uint32 {
-	return ((((((x & 0xFF) << 4) + ((x >> 8) & 0xFF)) << 4) + ((x >> 16) & 0xFF)) << 4) + ((x >> 24) & 0xFF)
+	// removed from preview
 }
 
 func weh(a uint32, b uint32) uint32 {
-	return ((a << 5) + (a >> 2) + b) & 0xFFFFFFFF
+	// removed from preview
 }
 
 func rotMess(x uint32) uint32 {
-
-	bss := new(bytes.Buffer)
-	_ = binary.Write(bss, binary.LittleEndian, x)
-
-	bs := bss.Bytes()
-	out := uint32(bs[0]) << uint32(11)
-	out = out | uint32(bs[1])
-	out ^= uint32(bs[0]) >> 5
-	out ^= 0xFFFFFFFF
-	out ^= uint32(bs[0])
-	a := (out << 7) & 0xFFFFFFFF
-	a ^= uint32(bs[2])
-	out ^= (out >> 3) ^ a
-
-	a = (out << 11) & 0xFFFFFFFF
-	a ^= uint32(bs[3])
-	out ^= ((out >> 5) ^ a) ^ 0xFFFFFFFF
-	return out & 0x7FFFFFFF
+	// removed from preview
 }
 
 // Endian swap
 func reverseBytes(x uint32) uint32 {
-	var y uint32
-	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.LittleEndian, x)
-	if err != nil {
-		fmt.Println("Error writing bytes: ", err)
-	}
-	b := bytes.NewReader(buf.Bytes())
-	_ = binary.Read(b, binary.BigEndian, &y)
-	return y
+	// removed from preview
 }
 
 func EncryptRB(srcData []byte) string {
